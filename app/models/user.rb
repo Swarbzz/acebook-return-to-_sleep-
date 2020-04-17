13cb60d4a5fea5b4b779314334ba5134fe1c73af
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-04-17 14:33:00 +0100

    destoy dislikes and profile pictures when user account gets deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
     has_many :likes, dependent: :destroy
+    has_many :dislikes, dependent: :destroy
     has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
     has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
 
     has_many :following, through: :active_relationships, source: :followed
     has_many :followers, through: :passive_relationships, source: :follower
     has_many :images
-    has_many :profilepictures
+    has_many :profilepictures, dependent: :destroy
 
 #      # follow another user
   def follow(other)
